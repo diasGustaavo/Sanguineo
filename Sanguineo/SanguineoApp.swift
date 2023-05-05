3de9c9e7d821b193aaf890ce119972314bf30be1
diasGustaavo <diasgustaavo@icloud.com>
2023-05-05 09:08:31 -0300

    Initial Commit

---
+++ Sanguineo/SanguineoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanguineoApp.swift
+//  Sanguineo
+//
+//  Created by Gustavo Dias on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SanguineoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
